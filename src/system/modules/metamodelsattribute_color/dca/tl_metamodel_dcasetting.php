505dc10bf185a03fa0ec074918e4e88a97933930
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-04-19 16:50:53 +0200

    Code style.

--- src/system/modules/metamodelsattribute_color/dca/tl_metamodel_dcasetting.php
+++ src/system/modules/metamodelsattribute_color/dca/tl_metamodel_dcasetting.php
@@ -18,12 +18,12 @@
 $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id']['color'] = array
 (
 	'presentation' => array(
-		'tl_class', 
+		'tl_class',
 	),
 	'functions'  => array(
 		'mandatory',
 	),
-	'overview' => array(		
+	'overview' => array(
 		'filterable',
 		'searchable',
 		'sortable',
